d3046427f5e8814ab53fbdbda61ad20184acdacf
MERCURTCOA <15931103962coa@gmail.com>
2018-12-13 16:16:36 -0500

    no change

--- server/config.js
+++ server/config.js
@@ -1,11 +1,11 @@
 const port = 9000;
-const secretKey = 'secretKey';
+const secretKey = "secretKey";
 const expiredAfter = 60 * 60 * 1000;
 
 export default {
   port,
   secretKey,
-  secret: 'a4f8071f-c873-4447-8ee2',
+  secret: "a4f8071f-c873-4447-8ee2",
   expiredAfter,
-  database: 'mongodb://localhost:27017/smartbeings',
+  database: "mongodb://localhost:27017/smartbeings"
 };
