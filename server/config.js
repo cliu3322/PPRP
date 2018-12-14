f27bf390ae97c99adb8fa531074f8c0d0b7559dc
MERCURTCOA <15931103962coa@gmail.com>
2018-12-13 23:53:04 -0500

    database config

--- server/config.js
+++ server/config.js
@@ -8,4 +8,5 @@ export default {
   secret: "a4f8071f-c873-4447-8ee2",
   expiredAfter,
   database: "mongodb://localhost:27017/smartbeings"
+  // database: "mongodb://mernusers:Aa1234@ds151383.mlab.com:51383/pprpapp"
 };
