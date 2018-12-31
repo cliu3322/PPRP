0feeb1ae1c2f595eefb3458d7e6f0dcd5d4d762e
C. Liu <cliu3322@hawaii.edu>
2018-12-30 21:30:45 -1000

    LOL, that was dumb

--- client/src/containers/PaperDisplay/index.js
+++ client/src/containers/PaperDisplay/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import LayoutContentWrapper from '../../components/utility/layoutWrapper';
-import LayoutContent from '../../components/utility/layoutContent';
-import PDF from 'react-pdf-js';
 
 
 export default class extends Component {
@@ -14,58 +12,16 @@ export default class extends Component {
     //console.log(this.state.contactOptions)
   }
 
-  onDocumentComplete = (pages) => {
-    this.setState({ page: 1, pages });
-  }
-
-  handlePrevious = () => {
-    this.setState({ page: this.state.page - 1 });
-  }
-
-  handleNext = () => {
-    this.setState({ page: this.state.page + 1 });
-  }
-
-  renderPagination = (page, pages) => {
-    let previousButton = <li className="previous" onClick={this.handlePrevious}><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
-    if (page === 1) {
-      previousButton = <li className="previous disabled"><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
-    }
-    let nextButton = <li className="next" onClick={this.handleNext}><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
-    if (page === pages) {
-      nextButton = <li className="next disabled"><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
-    }
-    return (
-      <nav>
-        <ul className="pager">
-          {previousButton}
-          {nextButton}
-        </ul>
-      </nav>
-      );
-  }
-
 
   render() {
 
-    let pagination = null;
-    if (this.state.pages) {
-      pagination = this.renderPagination(this.state.page, this.state.pages);
-    }
     return (
-      <LayoutContentWrapper style={{ height: '1000vh' }}>
-        <LayoutContent>
-          <h1>Blank Page</h1>
+      <LayoutContentWrapper >
           <div>
-            <PDF
-              width = {1000}
-              file="http://localhost:3000/pdf/aaa.pdf"
-              onDocumentComplete={this.onDocumentComplete}
-              page={this.state.page}
-            />
-            {pagination}
+            <object data="http://localhost:3000/pdf/aaa.pdf" type="application/pdf" width="800" height="800">
+              <p>Alternative text - include a link <a href="http://localhost:3000/pdf/aaa.pdf">to the PDF!</a></p>
+            </object>
           </div>
-        </LayoutContent>
       </LayoutContentWrapper>
     );
   }
